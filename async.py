4186f9ac82364656b9905c4c127774d111d96ba8
kotebeg <kotebeg@gmail.com>
2024-09-04 20:56:16 +0400

    adding enumerates

--- async.py
+++ async.py
@@ -4,4 +4,12 @@ Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
 # print(Dict)
 
 for key, value in Dict.items():
-    print(key, value)
+    # print(key, value)
+    pass
+
+
+# enumerate
+
+pylist = ["Geeks", "for", "Geeks"]
+for index, word in enumerate(pylist):
+    print(index, word)

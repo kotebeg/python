78bbbda0a1a1d545365015358c89fd549d3219cd
kotebeg <kotebeg@gmail.com>
2024-09-26 23:57:57 +0400

    adding virtual environment script

---
+++ virtualenv.sh
@@ -0,0 +1,6 @@
+# create virtual env under linux machine
+# create virtual evnironment on specific python version
+
+python3.12 -m venv path/to/venv
+python3.12 -m venv myenv
+source myenv/bin/activate
